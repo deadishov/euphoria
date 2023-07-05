f1737a1d3afd5926b94090702c95cc266975a582
Muhammad Dedishov <moxmad99@mail.ru>
2023-07-06 00:40:35 +0300

    Beginning...

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography>
+                    Heey
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+

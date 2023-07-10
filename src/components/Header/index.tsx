a32fd32a29f71858ab5f4dd4f64a2439e0d4a99e
Muhammad Dedishov <moxmad99@mail.ru>
2023-07-10 19:25:48 +0300

    Header is adaptive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 
-import { AppBar, Badge, Box, Toolbar } from '@mui/material'
-
-import logoSvg from '../../assets/img/logo.svg'
-import searchSvg from '../../assets/img/search.svg'
-import userSvg from '../../assets/img/user.svg'
-import favSvg from '../../assets/img/favorite.svg'
-import cartSvg from '../../assets/img/cart.svg'
-import styles from './Header.module.scss'
+import { AppBar, Badge, Box, InputBase, Toolbar } from '@mui/material'
+import { alpha, styled, useTheme } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { styled, useTheme } from '@mui/material/styles';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
@@ -20,8 +14,13 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+
+import logoSvg from '../../assets/img/logo.svg'
+import searchSvg from '../../assets/img/search.svg'
+import userSvg from '../../assets/img/user.svg'
+import favSvg from '../../assets/img/favorite.svg'
+import cartSvg from '../../assets/img/cart.svg'
+import styles from './Header.module.scss'
 
 const drawerWidth = 200;
 
@@ -32,6 +31,48 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
     justifyContent: 'flex-start',
+}));
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
 }));
 
 
@@ -142,7 +183,10 @@ export const Header = () => {
                     <Toolbar sx={{
                         color: '#000',
                         display: 'flex',
-                        justifyContent: 'space-between',
+                        justifyContent: {
+                            sm: 'space-between',
+                            xs: 'space-around'
+                        },
                         alignItems: 'center',
                         padding: 0
                     }}>
@@ -206,6 +250,16 @@ export const Header = () => {
                                 </ListItem>
                             ))}
                         </List>
+                        <Divider />
+                        <Search sx={{ mt: '10px' }}>
+                            <SearchIconWrapper>
+                                <SearchIcon sx={{ opacity: '0.3' }} />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
                     </Drawer>
                 </AppBar>
             </Box>

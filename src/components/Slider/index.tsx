36cfea2f04866fc2d89f4ec482bc531e6663b217
Muhammad Dedishov <moxmad99@mail.ru>
2023-07-22 23:49:41 +0300

    Slider is finished!

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,5 +1,4 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar } from 'swiper';
 import { useRef } from 'react';
 import { Swiper as SwiperType } from 'swiper';
 
@@ -10,6 +9,8 @@ import 'swiper/scss/scrollbar';
 import slidesArr from './slidesArr.json'
 import arrowRight from '../../assets/img/arrowRight.svg'
 import arrowLeft from '../../assets/img/arrowLeft.svg'
+import arrowRightDisabled from '../../assets/img/rightDisabled.svg'
+import arrowLeftDisabled from '../../assets/img/leftDisabled.svg'
 import './Slider.module.scss'
 import styles from './Slider.module.scss'
 import { Container } from '@mui/system';
@@ -17,35 +18,53 @@ import React from 'react';
 
 export const Slider = () => {
     const swiperRef = useRef<SwiperType>();
-    const [firstSlide, setFirstSlide] = React.useState(true)
+    const [slideCount, setSlideCount] = React.useState(1)
+
 
     const slideNext = () => {
         swiperRef.current?.slideNext()
-        setFirstSlide(false)
     }
 
     const slidePrev = () => {
         swiperRef.current?.slidePrev()
-        setFirstSlide(true)
     }
 
     return (
         <div>
-            <Swiper className={styles.swiperWrapper}
-                modules={[Navigation, Scrollbar]}
+            <Swiper
+                onSlideNextTransitionStart={() => setSlideCount(slideCount + 1)}
+                onSlidePrevTransitionStart={() => setSlideCount(slideCount - 1)}
+                className={styles.swiperWrapper}
                 onBeforeInit={(swiper) => {
                     swiperRef.current = swiper;
                 }}
-                // scrollbar={{ draggable: true }}
                 slidesPerView={1}
                 spaceBetween={50}
-
             >
                 {slidesArr.map((slide) => (
-                    <SwiperSlide style={{ backgroundImage: `url(${slide.image})`, display: 'flex', alignItems: 'center' }} className={styles.slide} key={slide.id}>
-                        <Container sx={{ display: 'flex', flexDirection: 'column', gap: '40px' }}>
+                    <SwiperSlide
+                        style={{
+                            backgroundImage: `url(${slide.image})`,
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}
+                        className={styles.slide}
+                        key={slide.id}>
+                        <Container
+                            className={styles.slideContent}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: '40px',
+                                maxWidth: {
+                                    xl: '1200px',
+                                    lg: '1000px',
+                                    md: '760px',
+                                    sm: '630px'
+                                }
+                            }}>
                             {slide.id === '1' && <h3 className={styles.slideTitle}>T-shirt / Tops</h3>}
-                            <h1 className={styles.slideHeadline}>{slide.id === '1' ? `Summer \n Value Pack` : `Don't miss\n SALES!`}</h1 >
+                            <h1 className={styles.slideHeadline}>{slide.id === '1' ? `Summer Value Pack` : `Don't miss SALES!`}</h1 >
                             {slide.id === '1' && <h3 className={styles.slideTitle}>cool / colorful / comfy</h3>}
                             <div>
                                 <button className={styles.slideButton}>{slide.id === '1' ? 'Shop Now' : `Let's go!`}</button>
@@ -54,15 +73,25 @@ export const Slider = () => {
                     </SwiperSlide>
                 ))}
                 <div className={styles.sliderNav}>
-                    <img className={styles.arrowLeft} src={arrowLeft} onClick={slidePrev} alt='arrow' />
-                    <img className={styles.arrowRight} src={arrowRight} onClick={slideNext} alt='arrow' />
+                    <img
+                        className={slideCount === 1 ? styles.arrowLeftDisabled : styles.arrowLeft}
+                        src={Number(slideCount) === 1 ? arrowLeftDisabled : arrowLeft}
+                        onClick={slidePrev}
+                        alt='arrow'
+                    />
+                    <img
+                        className={Number(slideCount) === slidesArr.length ? styles.arrowRightDisabled : styles.arrowRight}
+                        src={Number(slideCount) === slidesArr.length ? arrowRightDisabled : arrowRight}
+                        onClick={slideNext}
+                        alt='arrow'
+                    />
                 </div>
                 <div className={styles.scrollbarWrapper}>
                     <div className={styles.scrollbar}>
-                        <div style={firstSlide ? { transform: 'translateX(0)' } : { transform: 'translateX(100%)' }} className={styles.roller}></div>
+                        <div style={slideCount === 1 ? { transform: 'translateX(0)' } : { transform: 'translateX(100%)' }} className={styles.roller}></div>
                     </div>
                 </div>
             </Swiper>
-        </div>
+        </div >
     );
 };
